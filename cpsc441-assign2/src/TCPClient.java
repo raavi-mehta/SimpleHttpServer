fd92a580af7c15a71772ff1736ed10b95c954ef6
Raavi Mehta <raavism@gmail.com>
2016-10-29 19:52:50 -0600

    Add HttpRequestParser

--- cpsc441-assign2/src/TCPClient.java
+++ cpsc441-assign2/src/TCPClient.java
@@ -1,8 +1,8 @@
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 /**
  * A Simple Client
@@ -12,9 +12,9 @@ public class TCPClient implements Constants {
 	public static void main(String[] args) {
 
 		String s, tmp;
-		Scanner inputStream;
+		BufferedReader inputStream;
 		PrintWriter outputStream;
-		Scanner userinput;
+		BufferedReader userinput;
 
 		try {
 			// connects to port server app listesing at port 8888 in the same
@@ -24,21 +24,20 @@ public class TCPClient implements Constants {
 			// Create necessary streams
 			outputStream = new PrintWriter(new DataOutputStream(
 					socket.getOutputStream()));
-			inputStream = new Scanner(new InputStreamReader(
-					socket.getInputStream()));
-			userinput = new Scanner(System.in);
+			inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			userinput = new BufferedReader(new InputStreamReader(System.in));
 
 			// send/receive messages to/from server
 			while (true) {
 				System.out.println("Enter Text Message for Echo Server: ");
-				tmp = userinput.nextLine();
+				tmp = userinput.readLine();
 
 				// Send user input message to server
 				outputStream.println(tmp);
 				// Flush to make sure message is send
 				outputStream.flush();
 
-				s = inputStream.nextLine();
+				s = inputStream.readLine();
 				System.out.println(s);
 
 				// Exit if message from server is "bye"
